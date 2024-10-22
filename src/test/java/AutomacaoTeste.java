35ef2f5becc9be46a353354a68ef9a7be7b26449
Gabriel <gabrielcavalcanteteixeira@gmail.com>
2024-10-22 17:13:24 -0300

    automacao funcionando

--- src/test/java/AutomacaoTeste.java
+++ src/test/java/AutomacaoTeste.java
@@ -2,19 +2,44 @@ import Pages.PageGoogle;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class AutomacaoTeste
+public class AutomacaoTeste extends Hooks
 {
     @Test
+    @DisplayName("Cenario senha e email correto")
     public void tet() throws InterruptedException
     {
         PageGoogle braco = new PageGoogle();
-        braco.abrirnavegador();
+        braco.abrirsite("http://bug.dev.hill/signin");
         braco.maximizar();
-        braco.clicarEescrever1("APjFqb","A igreja de jesus cristo");
-        braco.clicar3("gNO89b");
-        braco.clicar2("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/div/div/div/div[1]/span");
-        braco.esperar(1000);
-        braco.clicar("truste-consent-button");
+        braco.clicarEescrever1("email","teixeirinha@local.dev");
+        braco.clicarEescrever1("pass","qwe123");
+        braco.clicar("login");
+        braco.esperar(3000);
+        braco.fecharnavegador();
+    }
+    @Test
+    @DisplayName("Cenario senha incorreta e email correto")
+    public void tet2() throws InterruptedException
+    {
+        PageGoogle braco = new PageGoogle();
+        braco.abrirsite("http://bug.dev.hill/signin");
+        braco.maximizar();
+        braco.clicarEescrever1("email","teixeirinha@local.dev");
+        braco.clicarEescrever1("pass","tplink");
+        braco.clicar("login");
+        braco.esperar(3000);
+        braco.fecharnavegador();
+    }
+    @Test
+    @DisplayName("Cenario senha correta e email incorreto")
+    public void tet3() throws InterruptedException
+    {
+        PageGoogle braco = new PageGoogle();
+        braco.abrirsite("http://bug.dev.hill/signin");
+        braco.maximizar();
+        braco.clicarEescrever1("email","gabrielcavalcanteteixeira@gmail.com");
+        braco.clicarEescrever1("pass","qwe123");
+        braco.clicar("login");
         braco.esperar(3000);
         braco.fecharnavegador();
     }
--- src/test/java/Hooks.java
+++ src/test/java/Hooks.java
@@ -1,6 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,14 +12,19 @@ public class Hooks
     public static WebDriver driver;
     public static ChromeOptions options;
 
-    @BeforeAll
-    public static void abrir()
+    @BeforeEach
+    public void abrir()
     {
         WebDriverManager.chromedriver().setup();
-        driver.get("https://www.google.com/");
+        options = new ChromeOptions();
+        options.addArguments(("--disable-crash-reporter"));
+        options.addArguments(("--disable-extensions"));
+        options.addArguments(("--disable-dev-shm"));
+        options.addArguments(("--disable-notifications"));
+        //driver.get("https://www.google.com/");
     }
-    @AfterAll
-    public static void fechar()
+    @AfterEach
+    public void fechar()
     {
         driver.quit();
     }
